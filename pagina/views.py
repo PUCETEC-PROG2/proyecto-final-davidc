c57f24e183241951817bc6a40b12d6cdfafe47fe
David <davidusa1en1@gmail.com>
2025-02-11 11:11:08 -0500

    MAIN (No subir nada aqui)

--- pagina/views.py
+++ pagina/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required, user_passes_test
+from .models import Propiedad
 
 def home(request):
+    propiedades_recientes = Propiedad.objects.all().order_by("-id")[:5]
+    # Opcional: para depurar, puedes imprimir el conteo
+    print("Propiedades recientes:", propiedades_recientes.count())
+    return render(request, "home.html", {"propiedades_recientes": propiedades_recientes})
+
+
     return render(request, 'home.html') 
 
 def is_staff(user):
